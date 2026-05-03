ade7dbff69150c614e92ae50ea9a0e4b7d2ed410
MohammadOb8 <obmohammad51@gmail.com>
2026-05-03 21:10:07 +0300

    Add Problem 33-2 Fill Array With Random Keys as Elements Solution

---
+++ MySolutionCode/MySolutionCode/Problem_21-2_GenerateKeys.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef PROBLEM_21_2_GenerateKeys
+#define PROBLEM_21_2_GenerateKeys
+#include <string>
+std::string GenerateKey(int NumOfKeys);
+void GeneratKeys(short NumOfKeys);
+
+#endif
---
+++ MySolutionCode/MySolutionCode/Problem_33-2_FillArrayWithKeys.cpp
@@ -0,0 +1,38 @@
+#include <iostream>   
+#include <string>   
+#include <cstdlib>    
+#include <ctime>  
+#include "Problem_0-2_ReadNum.h"
+#include "Problem_21-2_GenerateKeys.h"
+#include "Problem_23-2_RandomArrayElements.h"
+#include "Problem_28-2_CopyArray.h"
+using namespace std;
+
+void Generate_Random_Array_Keys(string arr[], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = GenerateKey(1);
+    }
+}
+
+void PrintStringArray(string arr[100], int arrLength)
+{
+
+    for (int i = 0; i < arrLength; i++)
+        cout << "Array["<< i <<"]: "<< arr[i] << endl;
+
+    cout << "\n";
+}
+
+int main() {
+
+    srand((unsigned)time(NULL));
+    string Array[100];
+    int arrLength = ReadNum("Enter Number of Keys: ");
+
+    Generate_Random_Array_Keys(Array, arrLength);
+    cout << "\nArray 1 Keys elements:\n";
+    PrintStringArray(Array, arrLength);
+
+}
